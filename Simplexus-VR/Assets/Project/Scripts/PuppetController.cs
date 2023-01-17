9b64ab7cab0cd92ece28dc114780d366a65d37fd
nuclearmayhem1 <53118215+nuclearmayhem1@users.noreply.github.com>
2023-01-17 14:43:34 +0100

    fixed vectors?

--- Simplexus-VR/Assets/Project/Scripts/PuppetController.cs
+++ Simplexus-VR/Assets/Project/Scripts/PuppetController.cs
@@ -19,6 +19,8 @@ public class PuppetController : MonoBehaviour
 
     public float moveSpeed = 1, turnSpeed = 1;
 
+    public float gravity = 1;
+
     private void Update()
     {
         Vector2 dirMove = inputMove.action.ReadValue<Vector2>();
@@ -28,8 +30,11 @@ public class PuppetController : MonoBehaviour
         bool isJumping = inputJump.action.ReadValue<bool>();
 
         //Move
-        character.Move(dirMove * moveSpeed);
-        transform.Rotate(dirTurn * turnSpeed);
+        Vector3 moveVector = new Vector3(dirMove.x, 0 + gravity, dirMove.y);
+        Vector3 turnVector = new Vector3(dirTurn.x, 0, 0);
+
+        character.Move(moveVector * moveSpeed);
+        transform.Rotate(turnVector * turnSpeed);
 
         leftHandTransform.localPosition = posLeftHand;
         rightHandTransform.localPosition = posRightHand;
