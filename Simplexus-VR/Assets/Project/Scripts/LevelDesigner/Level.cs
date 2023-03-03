bfb17789882e88547dd86c017b32fed738c9756b
nuclearmayhem1 <53118215+nuclearmayhem1@users.noreply.github.com>
2023-03-03 16:47:10 +0100

    Walltool Adjust and destroy mode

--- Simplexus-VR/Assets/Project/Scripts/LevelDesigner/Level.cs
+++ Simplexus-VR/Assets/Project/Scripts/LevelDesigner/Level.cs
@@ -52,6 +52,7 @@ public class Level : MonoBehaviour
                 incomingPoles = new List<GameObject>(),
                 outgoingPoles = new List<GameObject>(),
                 outgoingWalls = new List<GameObject>(),
+                incomingWalls = new List<GameObject>(),
             };
 
             wallConnections[instantiatedPole] = connection;
@@ -73,15 +74,116 @@ public class Level : MonoBehaviour
                 incomingPoles = new List<GameObject>(),
                 outgoingPoles = new List<GameObject>(),
                 outgoingWalls = new List<GameObject>(),
+                incomingWalls = new List<GameObject>(),
             };
 
             connection.outgoingPoles.Add(lastPole);
             wallConnections[lastPole].incomingPoles.Add(instantiatedPole);
-            connection.outgoingWalls.Add(BuildWall(instantiatedPole.transform, lastPole.transform, wallPrefab));
-
+            GameObject newWall = BuildWall(instantiatedPole.transform, lastPole.transform, wallPrefab);
+            connection.outgoingWalls.Add(newWall);
+            wallConnections[lastPole].incomingWalls.Add(newWall);
             wallConnections[instantiatedPole] = connection;
 
             return instantiatedPole;
+        }
+    }
+
+    public List<GameObject> HighlightConnectedWalls(GameObject hoveredPole, Material poleMaterial, Material wallMaterial, Material hoveredMaterial)
+    {
+        if (wallMaterial == null)
+        {
+            wallMaterial = poleMaterial;
+        }
+
+        List<GameObject> selectedWalls = new List<GameObject>();
+        List<GameObject> selectedPoles = new List<GameObject>();
+
+        foreach (GameObject wall in wallConnections[hoveredPole].outgoingWalls)
+        {
+            selectedWalls.Add(wall);
+        }
+
+        foreach (GameObject wall in wallConnections[hoveredPole].incomingWalls)
+        {
+            selectedWalls.Add(wall);
+        }
+
+        foreach (GameObject pole in wallConnections[hoveredPole].outgoingPoles)
+        {
+            selectedPoles.Add(pole);
+        }
+
+        foreach (GameObject pole in wallConnections[hoveredPole].incomingPoles)
+        {
+            selectedPoles.Add(pole);
+        }
+
+        foreach (GameObject wall in selectedWalls)
+        {
+            MeshRenderer rend = wall.GetComponent<MeshRenderer>();
+            Material[] newMats = new Material[2];
+            newMats[0] = rend.materials[0];
+            newMats[1] = wallMaterial;
+            rend.materials = newMats;
+        }
+        foreach (GameObject pole in selectedPoles)
+        {
+            MeshRenderer rend = pole.GetComponent<MeshRenderer>();
+            Material[] newMats = new Material[2];
+            newMats[0] = rend.materials[0];
+            newMats[1] = poleMaterial;
+            rend.materials = newMats;
+        }
+        List<GameObject> returnedSelection = new List<GameObject>();
+        returnedSelection.AddRange(selectedPoles);
+        returnedSelection.AddRange(selectedWalls);
+
+        MeshRenderer rend2 = hoveredPole.GetComponent<MeshRenderer>();
+        Material[] newMats2 = new Material[2];
+        newMats2[0] = rend2.materials[0];
+        newMats2[1] = hoveredMaterial;
+        rend2.materials = newMats2;
+
+        returnedSelection.Add(hoveredPole);
+        return returnedSelection;
+    }
+
+    public void ClearAllMaterials()
+    {
+        List<GameObject> all = new List<GameObject>();
+        foreach (WallConnection connection in wallConnections.Values)
+        {
+            foreach (GameObject gameObject in connection.incomingPoles)
+            {
+                all.Add(gameObject);
+            }
+            foreach (GameObject gameObject in connection.incomingWalls)
+            {
+                all.Add(gameObject);
+            }
+            foreach (GameObject gameObject in connection.outgoingPoles)
+            {
+                all.Add(gameObject);
+            }
+            foreach (GameObject gameObject in connection.outgoingWalls)
+            {
+                all.Add(gameObject);
+            }
+        }
+        ClearMaterials(all);
+    }
+
+    public void ClearMaterials(List<GameObject> selection)
+    {
+        foreach (GameObject selected in selection)
+        {
+            if (selected != null)
+            {
+                MeshRenderer rend = selected.GetComponent<MeshRenderer>();
+                Material[] newMats = new Material[1];
+                newMats[0] = rend.materials[0];
+                rend.materials = newMats;
+            }
         }
     }
 
@@ -89,7 +191,9 @@ public class Level : MonoBehaviour
     {
         wallConnections[newPole].outgoingPoles.Add(lastPole);
         wallConnections[lastPole].incomingPoles.Add(newPole);
-        wallConnections[newPole].outgoingWalls.Add(BuildWall(newPole.transform, lastPole.transform, wallPrefab));
+        GameObject newWall = BuildWall(newPole.transform, lastPole.transform, wallPrefab);
+        wallConnections[newPole].outgoingWalls.Add(newWall);
+        wallConnections[lastPole].incomingWalls.Add(newWall);
     }
 
     public GameObject BuildWall(Transform a, Transform b, GameObject wallPrefab)
@@ -108,6 +212,72 @@ public class Level : MonoBehaviour
         return newWall;
     }
 
+    public void DestroySelected(GameObject selected)
+    {
+        if (selected.layer == 8)
+        {
+            WallConnection connection = wallConnections[selected];
+            foreach (GameObject wall in connection.outgoingWalls)
+            {
+                Destroy(wall);
+            }
+            foreach (GameObject wall in connection.incomingWalls)
+            {
+                Destroy(wall);
+            }
+            foreach (GameObject pole in connection.incomingPoles)
+            {
+                for (int i = 0; i < wallConnections[pole].outgoingPoles.Count; i++)
+                {
+                    if (wallConnections[pole].outgoingPoles[i] == selected)
+                    {
+                        wallConnections[pole].outgoingPoles.RemoveAt(i);
+                        wallConnections[pole].outgoingWalls.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+            foreach (GameObject pole in connection.outgoingPoles)
+            {
+                for (int i = 0; i < wallConnections[pole].incomingPoles.Count; i++)
+                {
+                    if (wallConnections[pole].incomingPoles[i] == selected)
+                    {
+                        wallConnections[pole].incomingPoles.RemoveAt(i);
+                        wallConnections[pole].incomingWalls.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+            Destroy(selected);
+        }
+        else if (selected.layer == 9)
+        {
+            foreach (WallConnection connection in wallConnections.Values)
+            {
+                for (int i = 0; i < connection.incomingWalls.Count; i++)
+                {
+                    if (connection.incomingWalls[i] == selected)
+                    {
+                        connection.incomingWalls.RemoveAt(i);
+                        connection.incomingPoles.RemoveAt(i);
+                        break;
+                    }
+                }
+                for (int i = 0; i < connection.outgoingWalls.Count; i++)
+                {
+                    if (connection.outgoingWalls[i] == selected)
+                    {
+                        connection.outgoingWalls.RemoveAt(i);
+                        connection.outgoingPoles.RemoveAt(i);
+                        break;
+                    }
+                }
+            }
+            Destroy(selected);
+        }
+    }
+
 }
 
 
@@ -117,4 +287,5 @@ public struct WallConnection
     public List<GameObject> incomingPoles;
     public List<GameObject> outgoingPoles;
     public List<GameObject> outgoingWalls;
+    public List<GameObject> incomingWalls;
 }
--- Simplexus-VR/Assets/Project/Scripts/LevelDesigner/WallTool.cs
+++ Simplexus-VR/Assets/Project/Scripts/LevelDesigner/WallTool.cs
@@ -9,41 +9,47 @@ public class WallTool : XRGrabInteractable
     public GameObject polePrefab;
     public GameObject wallPrefab;
     public Transform hitPoint;
-
     public GameObject lastPole = null;
-
     private bool held = false;
     public LineRenderer lineRenderer;
     public Transform rayOrigin;
-
     public GameObject polePreview;
     public GameObject wallPreveiw;
-
     Level currentLevel = null;
-
     private bool canPlace = false;
-
     public Material previewMaterial;
     public Color wrong, correct;
     public float maxLenght = 1f, minLenght = 0.25f;
-
     public Material selectedMaterial;
-
     private Quaternion rotationAwayFromSurface = Quaternion.identity;
-
     public Dictionary<GameObject, WallConnection> wallConnections = null;
-
     private GameObject newPole = null;
-
     private GameObject hoveredPole = null;
+    private GameObject hoveredWall = null;
+    private static readonly string[] buildingLayers = { "Terrain", "Pole" };
+    private static readonly string[] destroyLayers = { "Terrain", "Pole", "Wall" };
+    private static readonly string[] adjustLayers = { "Terrain" };
+    public Material destroyPole, destroyWall, destroyHovered;
+    public Material adjustPole, adjustWall, adjustHovered;
 
-    private static readonly string[] checkedLayers = { "Terrain", "Pole" };
+    private List<GameObject> currentSelection = new List<GameObject>();
+
+    private GameObject adjustedPole = null;
+
+    public Material incorrectMat, correctMat;
+
+    public WallToolMode mode = WallToolMode.Build;
+    public void SetMode(WallToolMode newMode)
+    {
+        mode = newMode;
+    }
+
 
     private void Update()
     {
-        if (held)
+        if (held && mode == WallToolMode.Build)
         {
-            if (Physics.Raycast(rayOrigin.position, rayOrigin.forward, out RaycastHit hitInfo, 10, LayerMask.GetMask(checkedLayers)))
+            if (Physics.Raycast(rayOrigin.position, rayOrigin.forward, out RaycastHit hitInfo, 10, LayerMask.GetMask(buildingLayers)))
             {
                 if (hitInfo.collider.gameObject.layer == 6)
                 {
@@ -107,6 +113,14 @@ public class WallTool : XRGrabInteractable
                     hoveredPole.GetComponent<MeshRenderer>().materials = newMaterials;
                     hoveredPole = null;
                 }
+                if (hoveredWall != null)
+                {
+                    Material[] newMaterials = new Material[1];
+
+                    newMaterials[0] = hoveredPole.GetComponent<MeshRenderer>().materials[0];
+                    hoveredWall.GetComponent<MeshRenderer>().materials = newMaterials;
+                    hoveredWall = null;
+                }
                 lineRenderer.enabled = false;
                 polePreview.SetActive(false);
                 wallPreveiw.SetActive(false);
@@ -115,22 +129,232 @@ public class WallTool : XRGrabInteractable
                 canPlace = false;
             }
         }
-        else
+        else if (mode == WallToolMode.Destroy)
         {
-            lineRenderer.enabled = false;
             polePreview.SetActive(false);
             wallPreveiw.SetActive(false);
-            currentLevel = null;
-            wallConnections = null;
-            canPlace = false;
-            if (hoveredPole != null)
-            {
-                Material[] newMaterials = new Material[1];
 
-                newMaterials[0] = hoveredPole.GetComponent<MeshRenderer>().materials[0];
-                hoveredPole.GetComponent<MeshRenderer>().materials = newMaterials;
-                hoveredPole = null;
+            if (Physics.Raycast(rayOrigin.position, rayOrigin.forward, out RaycastHit hitInfo, 10, LayerMask.GetMask(destroyLayers)))
+            {
+                if (hoveredPole != null && hitInfo.collider.gameObject != hoveredPole)
+                {
+                    if (currentSelection.Count != 0)
+                    {
+                        hitInfo.transform.GetComponentInParent<Level>().ClearMaterials(currentSelection);
+                        currentSelection = new List<GameObject>();
+                        hoveredPole = null;
+                    }
+                }
+                if (hoveredWall != null && hitInfo.collider.gameObject != hoveredWall)
+                {
+                    if (currentSelection.Count != 0)
+                    {
+                        hitInfo.transform.GetComponentInParent<Level>().ClearMaterials(currentSelection);
+                        currentSelection = new List<GameObject>();
+                        hoveredWall = null;
+                    }
+                }
+
+                if (hitInfo.collider.gameObject.layer == 6)
+                {
+                    currentLevel = hitInfo.transform.GetComponentInParent<Level>();
+                    wallConnections = currentLevel.wallConnections;
+                    hitPoint.position = hitInfo.collider.ClosestPoint(hitInfo.point);
+                    rotationAwayFromSurface = Quaternion.LookRotation(hitPoint.position - hitInfo.transform.position) * Quaternion.Euler(90, 0, 0);
+                    hitPoint.rotation = rotationAwayFromSurface;
+                    lineRenderer.enabled = true;
+                    lineRenderer.SetPosition(0, rayOrigin.position);
+                    lineRenderer.SetPosition(1, hitPoint.position);
+                }
+                else if (hitInfo.collider.gameObject.layer == 8)
+                {
+                    currentLevel = hitInfo.transform.GetComponentInParent<Level>();
+                    wallConnections = currentLevel.wallConnections;
+                    lineRenderer.enabled = true;
+                    hoveredPole = hitInfo.collider.gameObject;
+                    lineRenderer.SetPosition(0, rayOrigin.position);
+                    lineRenderer.SetPosition(1, hoveredPole.transform.position);
+                    currentSelection = currentLevel.HighlightConnectedWalls(hoveredPole, destroyPole, destroyWall, destroyHovered);
+                    hoveredWall = null;
+                }
+                else if (hitInfo.collider.gameObject.layer == 9)
+                {
+                    currentLevel = hitInfo.transform.GetComponentInParent<Level>();
+                    wallConnections = currentLevel.wallConnections;
+                    lineRenderer.enabled = true;
+                    hoveredWall = hitInfo.collider.gameObject;
+                    lineRenderer.SetPosition(0, rayOrigin.position);
+                    lineRenderer.SetPosition(1, hoveredWall.transform.position);
+                    hoveredPole = null;
+                    MeshRenderer rend = hoveredWall.GetComponent<MeshRenderer>();
+                    Material[] newMats = new Material[2];
+                    newMats[0] = rend.materials[0];
+                    newMats[1] = destroyWall;
+                    rend.materials = newMats;
+                    currentSelection.Add(hoveredWall);
+                }
+
             }
+            else
+            {
+                lineRenderer.enabled = false;
+                polePreview.SetActive(false);
+                wallPreveiw.SetActive(false);
+                currentLevel = null;
+                wallConnections = null;
+                canPlace = false;
+                hoveredPole = null;
+                hoveredWall = null;
+                if (currentSelection.Count != 0)
+                {
+                    hitInfo.transform.GetComponentInParent<Level>().ClearMaterials(currentSelection);
+                    currentSelection = new List<GameObject>();
+                }
+            }
+        }
+        else if (mode == WallToolMode.Adjust)
+        {
+            polePreview.SetActive(false);
+            wallPreveiw.SetActive(false);
+
+            if (Physics.Raycast(rayOrigin.position, rayOrigin.forward, out RaycastHit hitInfo, 10, LayerMask.GetMask(adjustedPole == null? buildingLayers : adjustLayers)))
+            {
+                if (adjustedPole == null)
+                {
+                    if (hoveredPole != null && hitInfo.collider.gameObject != hoveredPole)
+                    {
+                        if (currentSelection.Count != 0)
+                        {
+                            hitInfo.transform.GetComponentInParent<Level>().ClearMaterials(currentSelection);
+                            currentSelection = new List<GameObject>();
+                            hoveredPole = null;
+                        }
+                    }
+
+                    if (hitInfo.collider.gameObject.layer == 6)
+                    {
+                        currentLevel = hitInfo.transform.GetComponentInParent<Level>();
+                        wallConnections = currentLevel.wallConnections;
+                        hitPoint.position = hitInfo.collider.ClosestPoint(hitInfo.point);
+                        rotationAwayFromSurface = Quaternion.LookRotation(hitPoint.position - hitInfo.transform.position) * Quaternion.Euler(90, 0, 0);
+                        hitPoint.rotation = rotationAwayFromSurface;
+                        lineRenderer.enabled = true;
+                        lineRenderer.SetPosition(0, rayOrigin.position);
+                        lineRenderer.SetPosition(1, hitPoint.position);
+                    }
+                    else if (hitInfo.collider.gameObject.layer == 8)
+                    {
+                        currentLevel = hitInfo.transform.GetComponentInParent<Level>();
+                        wallConnections = currentLevel.wallConnections;
+                        lineRenderer.enabled = true;
+                        hoveredPole = hitInfo.collider.gameObject;
+                        lineRenderer.SetPosition(0, rayOrigin.position);
+                        lineRenderer.SetPosition(1, hoveredPole.transform.position);
+                        currentSelection = currentLevel.HighlightConnectedWalls(hoveredPole, adjustPole, adjustWall, adjustHovered);
+                        hoveredWall = null;
+                    }
+                    else if (hitInfo.collider.gameObject.layer == 9)
+                    {
+                        currentLevel = hitInfo.transform.GetComponentInParent<Level>();
+                        wallConnections = currentLevel.wallConnections;
+                        lineRenderer.enabled = true;
+                        hoveredPole = null;
+                        lineRenderer.SetPosition(0, rayOrigin.position);
+                        lineRenderer.SetPosition(1, hitPoint.position);
+                    }
+                }
+                else
+                {
+                    if (hitInfo.collider.gameObject.layer == 6)
+                    {
+                        currentLevel = hitInfo.transform.GetComponentInParent<Level>();
+                        wallConnections = currentLevel.wallConnections;
+
+                        hitPoint.position = hitInfo.collider.ClosestPoint(hitInfo.point);
+                        rotationAwayFromSurface = Quaternion.LookRotation(hitPoint.position - hitInfo.transform.position) * Quaternion.Euler(90, 0, 0);
+                        hitPoint.rotation = rotationAwayFromSurface;
+                        hitPoint.Translate(new Vector3(0, polePrefab.transform.localScale.y, 0), Space.Self);
+                        lineRenderer.enabled = true;
+                        lineRenderer.SetPosition(0, rayOrigin.position);
+                        lineRenderer.SetPosition(1, hitPoint.position);
+                        AdjustPreview();
+                    }
+                    else
+                    {
+                        lineRenderer.enabled = true;
+                        lineRenderer.SetPosition(0, rayOrigin.position);
+                        lineRenderer.SetPosition(1, hitPoint.position);
+                    }
+                }
+            }
+            else
+            {
+                lineRenderer.enabled = false;
+                polePreview.SetActive(false);
+                wallPreveiw.SetActive(false);
+                currentLevel = null;
+                wallConnections = null;
+                canPlace = false;
+                hoveredPole = null;
+                hoveredWall = null;
+            }
+        }
+    }
+    private bool canAdjust = false;
+    public void AdjustPreview()
+    {
+        WallConnection connection = currentLevel.wallConnections[adjustedPole];
+
+        canAdjust = true;
+
+        adjustedPole.transform.position = hitPoint.position;
+        adjustedPole.transform.rotation = hitPoint.rotation;
+
+        for (int i = 0; i < connection.incomingPoles.Count; i++)
+        {
+            if (!PreviewWall(connection.incomingWalls[i], connection.incomingPoles[i]))
+            {
+                canAdjust = false;
+            }
+        }
+        for (int i = 0; i < connection.outgoingPoles.Count; i++)
+        {
+            if (!PreviewWall(connection.outgoingWalls[i], connection.outgoingPoles[i]))
+            {
+                canAdjust = false;
+            }
+        }
+
+    }
+
+    public bool PreviewWall(GameObject wall, GameObject pole)
+    {
+        Vector3 averagePos = (adjustedPole.transform.position + pole.transform.position) / 2;
+        wall.transform.position = averagePos;
+        Vector3 wallSize = wallPrefab.transform.localScale;
+        float distance = Vector3.Distance(adjustedPole.transform.position, pole.transform.position);
+        wallSize.z = distance;
+
+        wall.transform.rotation = Quaternion.LookRotation(pole.transform.position - adjustedPole.transform.position, adjustedPole.transform.up);
+        wall.transform.localScale = wallSize;
+
+        if (distance > maxLenght || distance < minLenght)
+        {
+            MeshRenderer rend = wall.GetComponent<MeshRenderer>();
+            Material[] newMats = new Material[2];
+            newMats[0] = rend.materials[0];
+            newMats[1] = incorrectMat;
+            rend.sharedMaterials = newMats;
+            return false;
+        }
+        else
+        {
+            MeshRenderer rend = wall.GetComponent<MeshRenderer>();
+            Material[] newMats = new Material[2];
+            newMats[0] = rend.materials[0];
+            newMats[1] = correctMat;
+            rend.sharedMaterials = newMats;
+            return true;
         }
     }
 
@@ -219,28 +443,69 @@ public class WallTool : XRGrabInteractable
     protected override void OnActivated(ActivateEventArgs args)
     {
         base.OnActivated(args);
-
-        if (canPlace)
+        if (mode == WallToolMode.Build)
         {
-            lastPole = currentLevel.PlacePole(lastPole, newPole, wallPrefab, hitPoint);
-        }
-        else
-        {
-            if (hoveredPole != null)
+            if (canPlace)
             {
-                lastPole = hoveredPole;
-                Material[] newMaterials = new Material[1];
-
-                newMaterials[0] = hoveredPole.GetComponent<MeshRenderer>().materials[0];
-                hoveredPole.GetComponent<MeshRenderer>().materials = newMaterials;
-                hoveredPole = null;
+                lastPole = currentLevel.PlacePole(lastPole, newPole, wallPrefab, hitPoint);
             }
             else
             {
-                lastPole = null;
+                if (hoveredPole != null)
+                {
+                    lastPole = hoveredPole;
+                    Material[] newMaterials = new Material[1];
+
+                    newMaterials[0] = hoveredPole.GetComponent<MeshRenderer>().materials[0];
+                    hoveredPole.GetComponent<MeshRenderer>().materials = newMaterials;
+                    hoveredPole = null;
+                }
+                else
+                {
+                    lastPole = null;
+                }
             }
         }
-
+        else if (mode == WallToolMode.Destroy)
+        {
+            if (hoveredPole != null)
+            {
+                currentLevel.ClearAllMaterials();
+                currentLevel.DestroySelected(hoveredPole);
+                currentSelection = new List<GameObject>();
+                hoveredPole = null;
+            }
+            else if (hoveredWall != null)
+            {
+                currentLevel.ClearAllMaterials();
+                currentLevel.DestroySelected(hoveredWall);
+                currentSelection = new List<GameObject>();
+                hoveredWall = null;
+            }
+        }
+        else if (mode == WallToolMode.Adjust)
+        {
+            if (adjustedPole == null)
+            {
+                if (hoveredPole != null)
+                {
+                    adjustedPole = hoveredPole;
+                }
+            }
+            else
+            {
+                if (canAdjust)
+                {
+                    adjustedPole = null;
+                    hoveredPole = null;
+                }
+                else
+                {
+                    currentLevel.ClearAllMaterials();
+                    currentLevel.DestroySelected(hoveredPole);
+                }
+            }
+        }
     }
     protected override void OnDeactivated(DeactivateEventArgs args)
     {
@@ -266,4 +531,9 @@ public class WallTool : XRGrabInteractable
 
 
 
+}
+
+public enum WallToolMode
+{
+    Build, Adjust, Destroy
 }
